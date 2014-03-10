a9b2a81bf9c560c96dbf87f912750ba73bf4cc0a
brownhead <jsull003@ucr.edu>
2014-03-09 23:52:06 -0700

    Added don't watch list.

--- phial/tool.py
+++ phial/tool.py
@@ -53,9 +53,22 @@ def parse_arguments(args = sys.argv[1:]):
                 "behavior can be disabled with --no-watch-defaults."
         ),
         make_option(
+            "-W", "--dont-watch", action = "append", dest = "dont_watch_list",
+            default = [],
+            help =
+                "Adds a file or directory to the don't watch list. The path "
+                "provided will be globbed every time the list is checked. "
+                "If a file or directory exists in both the watch list and the "
+                "don't watch list, it will not be watched. By default, the "
+                "output directory will be in the unwatch list. This default "
+                "behavior can be disabled with --no-watch-defaults."
+        ),
+        make_option(
             "--no-watch-defaults", action = "store_false", default = True,
             dest = "watch_defaults",
-            help = "Do not populate the watch list with the default items"
+            help =
+                "Do not populate the watch list or the don't-watch list with "
+                "the default items"
         ),
         make_option(
             "--watch-poll-frequency", action = "store", default = "1",
@@ -110,7 +123,7 @@ def setup_logging(verbose):
 
     logging.basicConfig(level = log_level, format = format)
 
-def get_state_token(dir_paths):
+def get_state_token(dir_paths, exceptions):
     """
     This function will iterate through the names and timestamps on every file
     in the given directories and return a unique hash of that information.
@@ -119,6 +132,9 @@ def get_state_token(dir_paths):
     the same directories, they will only be different if a file was added or
     removed, a file was updated, a file was renamed, or a directory was
     added/removed/renamed.
+
+    Any item that exists in the exceptions list will be ignored. The format of
+    the exceptions list is the same as for dir_paths.
 
     """
 
@@ -135,13 +151,31 @@ def get_state_token(dir_paths):
             for j in os.walk(i, topdown = True):
                 yield j
 
+    def prune_paths(paths, exceptions_set, root = "."):
+        return [i for i in paths
+            if os.path.abspath(os.path.join(root, i)) not in exceptions_set]
+
+    # Glob and get a canonical path for each of the exceptions
+    exceptions = expand_globs(exceptions)
+    exceptions = set(os.path.abspath(i) for i in exceptions)
+
+    # Prune the directory list of any exceptions
+    dir_paths = prune_paths(dir_paths, exceptions)
+
+    # Glob all the paths in the directory list
+    globbed_paths = expand_globs(dir_paths)
+
     hasher = hashlib.md5()
-    globbed_paths = list(expand_globs(dir_paths))
     for root, dirs, files in walk_many(globbed_paths):
         # Setting topdown to True above allows us to modify the directory list
         # in dirs. The walk function will visit each item in the order in which
         # they appear in that list. So here we sort it to make sure that the
-        # order in which they are visited is well defined.
+        # order in which they are visited is well defined, we also make sure
+        # that none of the directories are in our exceptions list or hidden.
+        # Note that we need to do slice assignment to ensure that we're
+        # affecting the original list.
+        dirs[:] = prune_paths(dirs, exceptions, root)
+        dirs[:] = [i for i in dirs if not i.startswith(".")]
         dirs.sort()
 
         # Create a list of the directory's absolute paths (this is a list
@@ -159,7 +193,7 @@ def get_state_token(dir_paths):
 
         # Iterate through all the files in sorted order (so we always visit
         # them in the same order between different runs of this function).
-        for i in sorted(files):
+        for i in sorted(prune_paths(files, exceptions)):
             # Get the absolute path of the file
             cur_file_path = os.path.join(root, i)
 
@@ -171,22 +205,23 @@ def get_state_token(dir_paths):
 
     return hasher.digest()
 
-def monitor(watch_list, wait_time, callback):
+def monitor(watch_list, dont_watch_list, wait_time, callback):
     """
     Loops forever and runs callback whenever a change is detected in the watch
     list.
 
     """
 
-    log.info("Entering monitor mode. Watch list: %r.", watch_list)
+    log.info("Entering monitor mode. Watch list: %r. Don't watch list: %r.",
+        watch_list, dont_watch_list)
 
-    old_token = get_state_token(watch_list)
+    old_token = get_state_token(watch_list, dont_watch_list)
 
     while True:
         while True:
             time.sleep(wait_time)
 
-            current_token = get_state_token(watch_list)
+            current_token = get_state_token(watch_list, dont_watch_list)
             if old_token != current_token:
                 break
         old_token = current_token
@@ -288,6 +323,7 @@ def _main(options, arguments):
         arguments, vars(options))
 
     watch_list = list(options.watch_list)
+    dont_watch_list = list(options.dont_watch_list)
     if options.watch_defaults:
         # Add the directory of the application. If the user provided a path
         # like `app.py` dirname will give us an empty string, so translate that
@@ -298,6 +334,8 @@ def _main(options, arguments):
         watch_list.append(app_dir)
 
         watch_list.append(options.source)
+
+        dont_watch_list.append(options.output)
 
     # We'll pass this callback function to our monitor routine
     callback = lambda: fork_and_build_app(app_path, options.source,
@@ -312,4 +350,5 @@ def _main(options, arguments):
             int(options.serve_port), options.verbose)
 
     if options.monitor:
-        monitor(watch_list, float(options.watch_poll_frequency), callback)
+        monitor(watch_list, dont_watch_list,
+            float(options.watch_poll_frequency), callback)
