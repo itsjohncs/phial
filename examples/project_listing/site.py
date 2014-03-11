9ad9ceca3e614a587bcdb6d416ed0733d3edc293
brownhead <jsull003@ucr.edu>
2014-03-11 13:00:07 -0700

    Removed outdated examples.

--- examples/project_listing/site.py
+++
@@ -1,9 +0,0 @@
-from phial import *
-
-@page("projects/<name>", files = "projects/*.md")
-def projects(project):
-    return render_template(project)
-
-@page("projects"):
-def project_listing():
-    return render_template(File("projects.htm"))
