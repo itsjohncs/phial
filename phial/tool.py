itsjohncs/phial

commit 35312739815d9e3891b2204ad7c779a3169ca6b8

Changed default value of --source for #14.

phial/tool.py
@@ -49,8 +49,8 @@ def parse_arguments(args = sys.argv[1:]):
     parser.add_option(
         "-s", "--source", action = "store", default = None,
         help =
-            "The directory the source files are in. Defaults to ./site if it "
-            "exists, otherwise it defaults to your current directory."
+            "The directory the source files are in. Defaults to ./content if "
+            "it exists, otherwise it defaults to your current directory."
     )
     parser.add_option(
         "-o", "--output", action = "store", default = "./output",
@@ -412,8 +412,8 @@ def _main(options, arguments, deletion_list):
         arguments, vars(options))
 
     if options.source is None:
-        if os.path.isdir("./site"):
-            options.source = "./site"
+        if os.path.isdir("./content"):
+            options.source = "./content"
         else:
             options.source = "."
 

commit 264a2016094196c2b75b5ad1ad3ab37429ca5321

Testing server address is now always displayed.
This was a regression caused by
35312739815d9e3891b2204ad7c779a3169ca6b8.

phial/tool.py
@@ -388,7 +388,9 @@ def fork_and_serve(public_dir, host, port, verbose):
         except KeyboardInterrupt:
             pass
 
-    log.info("Starting server at http://%s:%s", host, port)
+    # Print to stdout to ensure the user always recieves this message (without
+    # doing anything too clunky like logging an error).
+    print "Serving site at http://{}:{}".format(host, port)
     p = multiprocessing.Process(target = serve)
     p.daemon = True
     p.start()